3a5da9bddc249ae6aa40a671ac485ea81d2b7c7e
이정인 <dlwjddls0130@naver.com>
2022-11-26 20:14:24 +0900

    Recycle, Region, Attraction Api 푸시

--- app/src/main/java/com/example/myapplication/LandmarkAPI.java
+++ app/src/main/java/com/example/myapplication/LandmarkAPI.java
@@ -8,5 +8,14 @@ import retrofit2.http.Path;
 
 public interface LandmarkAPI {
     @GET("/landmark/{names}")
-    Call<List<LandmarkCongest>> getLandmarkInfo(@Path("names") String names);
+    Call<List<LandMark>> getLandmarkInfo(@Path("names") String names);
+
+    @GET("/landmark/recycle")
+    Call<List<List<LandMark>>> getRecycleLandmarkInfo();
+
+    @GET("/region")
+    Call<List<List<LandMark>>> getRegionLandmarkInfo();
+
+    @GET("/attraction")
+    Call<List<List<LandMark>>> getAttractionLandmarkInfo();
 }
